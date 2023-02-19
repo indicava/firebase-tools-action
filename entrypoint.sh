71992a80cf0d597a367f503eb429a83ef55756e4
Elad Altstein <indicava@gmail.com>
2023-02-19 14:37:38 +0200

    firebase-tools-action v1.93

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -n "$GCP_SA_KEY" ]; then
-  if echo "$GCP_SA_KEY" | jq empty 2>/dev/null; then
+  if cat "$GCP_SA_KEY" | jq empty 2>/dev/null; then
     echo "Storing GCP_SA_KEY in /opt/gcp_key.json"
-    echo "$GCP_SA_KEY" > /opt/gcp_key.json
+    cat "$GCP_SA_KEY" > /opt/gcp_key.json
   else
     echo "Storing the decoded GCP_SA_KEY in /opt/gcp_key.json"
     echo "$GCP_SA_KEY" | base64 -d > /opt/gcp_key.json # If encoded base64 key, decode and save
